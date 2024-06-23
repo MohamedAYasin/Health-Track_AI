63ca48f31b540b249cadf677cc0f9fd1e392db3c
aristidei <a.isingizwe@alustudent.com>
2024-06-24 00:42:04 +0200

    chat bot page and send message

---
+++ lib/models/message_model.dart
@@ -0,0 +1,6 @@
+class Message {
+  final String message;
+  final bool isSender;
+
+  Message({required this.message, required this.isSender});
+}
--- lib/pages/chat_bot_page.dart
+++ lib/pages/chat_bot_page.dart
@@ -1,17 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:health_track_ai/constant/colors.dart';
+import 'package:health_track_ai/models/message_model.dart';
+import 'package:health_track_ai/widgets/chat_bubble.dart';
+import 'package:health_track_ai/widgets/message_bar.dart';
 
-class ChatBotPage extends StatelessWidget {
+final List<Message> dummyMessages = List.generate(
+  10,
+  (index) => Message(
+    message: 'Message $index Hello World! This is a dummy message.',
+    isSender: index % 2 == 0,
+  ),
+);
+
+class ChatBotPage extends StatefulWidget {
   const ChatBotPage({super.key});
+
+  @override
+  State<ChatBotPage> createState() => _ChatBotPageState();
+}
+
+class _ChatBotPageState extends State<ChatBotPage> {
+  final List<Message> messages = List.from(dummyMessages);
+  final ScrollController _scrollController = ScrollController();
+
+  void _handleSend(String message) {
+    setState(() {
+      messages.add(Message(
+        message: message,
+        isSender: true,
+      ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Chat Bot'),
+        title: const Text('Chat Bot',
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: CustomColors.mainBlue,
+        foregroundColor: CustomColors.white,
       ),
-      body: const Center(
-        child: Text('Welcome to the Chat Bot Page'),
+      body: ListView.builder(
+        controller: _scrollController,
+        itemCount: messages.length,
+        reverse: true, // Reverses the list order
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 6.0),
+            child: ChatBubble(
+              message: messages[messages.length - 1 - index].message,
+              isSender: messages[messages.length - 1 - index].isSender,
+            ),
+          );
+        },
       ),
+      bottomNavigationBar: MessageBar(onSend: _handleSend),
     );
   }
 }
